1234d80efa31fca1b7358e6439126a1b2333386a
jayjay2397 <jj.valdez09@gmail.com>
2024-02-26 22:33:45 -0600

    finished the navbar component

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
 import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
+
+
 
 
 const HeroSection = () => {
@@ -7,30 +13,46 @@ const HeroSection = () => {
         <section>
             <div className="gird grid-cols-1 sm:gird-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
-                <h1 className="mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                   <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-orange-400">
-                    Hello, I'm{" "} </span> 
-                    Jose
-                </h1>
-                <p className="text-base sm:text-lg mb-6 lg:text-xl">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint placeat architecto esse praesentium nam perferendis quidem delectus cumque aliquam unde corrupti, ea alias! Dolorum aspernatur at dolore sapiente sed magni!
-                </p>
+                    <h1 className="mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-orange-400">
+                            Hello, I'm{" "}
+                        </span>
+                        <br />
+                        <TypeAnimation
+                            sequence={[
+                                'Jose',
+                                1000,
+                                'Technical Project Manager',
+                                1000,
+                                'Web Developer',
+                                1000,
+                                'Software Engineer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className="text-base sm:text-lg mb-6 lg:text-xl">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint placeat architecto esse praesentium nam perferendis quidem delectus cumque aliquam unde corrupti, ea alias! Dolorum aspernatur at dolore sapiente sed magni!
+                    </p>
                 </div>
                 <div>
-                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-orange-500 to-yellow-500 hover:bg-slate-200 text-white">Hire Me</button>
+                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-orange-500 to-yellow-500 hover:bg-slate-200 text-white">Contact Me</button>
                     <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-orange-500 to-yellow-500 hover:bg-slate-800 text-white mt-3">
-                     <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span> 
+                        <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</span>
                     </button>
                 </div>
-                <div className="col-span-5 place-self-center">
+                <div className="col-span-4 place-self-center mt-4 lg:mt-0">
                     <div className=" rounded-full bg-[#2b2b2b] w-[250px] h-[250px] lg:w-[400px] lg:h[400px] relative">
-                    <Image
-                     src="/images/memoji.png"
-                     alt="hero-image"
-                     className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                     width={300}
-                     height={300}
-                    />
+                        <Image
+                            src="/images/memoji.png"
+                            alt="hero-image"
+                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                            width={300}
+                            height={300}
+                        />
                     </div>
                 </div>
             </div>
@@ -39,4 +61,4 @@ const HeroSection = () => {
     );
 };
 
-export default HeroSection
+export default HeroSection;
